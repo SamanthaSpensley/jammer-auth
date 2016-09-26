6f826a3e44a556823f9aca822493d375b9fdf634
Samantha Spensley <spenslsm@gmail.com>
2016-09-25 19:51:36 -0600

    pushed to heroku, added dotenv and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+#Jammer Auth
+
+#####Practice setting up Google Auth 2.0
+
+###Live site
+[heroku](https://jammer-auth.herokuapp.com/)
--- knexfile.js
+++ knexfile.js
@@ -1,37 +1,19 @@
 // Update with your config settings.
 
+if(!process.env.NODE_ENV){
+  require('dotenv').config();
+}
+
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: 'postgres://localhost/jammer'
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
